c4270279e46b329952d04a226cfd8ddd1f039fb2
JonasStockkamp <125880331+JonasStockkamp@users.noreply.github.com>
2023-03-02 09:48:45 +0100

    Update __init__.py

--- capp/__init__.py
+++ capp/__init__.py
@@ -3,9 +3,7 @@ import os
 
 application = Flask(__name__)
 
-# application.config['SECRET_KEY'] = os.environ['SECRET_KEY']  
-
-application.config['SECRET_KEY'] = '3oueqkfdfas8ruewqndr8ewrewrouewrere44554'
+application.config['SECRET_KEY'] = os.environ['SECRET_KEY']  
 
 from capp.home.routes import home
 from capp.methodology.routes import methodology
@@ -16,4 +14,3 @@ application.register_blueprint(home)
 application.register_blueprint(methodology)
 application.register_blueprint(carbon_app)
 application.register_blueprint(users)
-
